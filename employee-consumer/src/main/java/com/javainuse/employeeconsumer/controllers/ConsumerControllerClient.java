1039cb21eb9db4ab61521d48d5c46f4bb11c2a9c
Vivek <vivekmuralidharan.ps@gmail.com>
2018-07-14 15:44:46 -0500

    Made changes to accomodate ribbon for load balancing

--- employee-consumer/src/main/java/com/javainuse/employeeconsumer/controllers/ConsumerControllerClient.java
+++ employee-consumer/src/main/java/com/javainuse/employeeconsumer/controllers/ConsumerControllerClient.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,13 +16,20 @@ import org.springframework.web.client.RestTemplate;
 
 public class ConsumerControllerClient {
 	
+/*	@Autowired
+	private DiscoveryClient discoveryClient;*/
+	
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	private LoadBalancerClient loadBalancer;
 	
 	public void getEmployee() throws RestClientException, IOException {
 
-		List<ServiceInstance> instances=discoveryClient.getInstances("employee-producer");
-		ServiceInstance serviceInstance=instances.get(0);
+/*		List<ServiceInstance> instances=discoveryClient.getInstances("employee-producer");
+		ServiceInstance serviceInstance=instances.get(0);*/
+		
+		ServiceInstance serviceInstance=loadBalancer.choose("employee-producer");
+		
+		System.out.println(serviceInstance.getUri());
 		
 		String baseUrl=serviceInstance.getUri().toString();
 		
